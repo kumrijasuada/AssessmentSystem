f6c81152343c3114f0a366bfb37bcf174e18cc3e
Ada <kumrijasuada@gmail.com>
2024-03-07 17:47:16 +0100

    refactor workflow file

---
+++ Assess/Services/Exam.cs
@@ -0,0 +1,13 @@
+﻿namespace Assess.Services
+{
+    public class Exam
+    {
+        public char GetMostFrequent(string input)
+        {
+            var charMap = input.Distinct()
+                .ToDictionary(c => c, c => input.Count(s => s == c));
+
+            return charMap.OrderByDescending(v => v.Value).First().Key;
+        }
+    }
+}
--- Assessment.Tests/UnitTest1.cs
+++ Assessment.Tests/UnitTest1.cs
@@ -1,11 +1,18 @@
+using Assess.Services;
+
 namespace Assessment.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetMostFrequentCharTest()
         {
+            Exam e = new();
 
+            Assert.Equal('a', e.GetMostFrequent("mamaaa"));
+            Assert.Equal('g', e.GetMostFrequent("gaga"));
+            Assert.Equal('a', e.GetMostFrequent("agag"));
+            Assert.Equal('3', e.GetMostFrequent("123123123456"));
         }
     }
 }
