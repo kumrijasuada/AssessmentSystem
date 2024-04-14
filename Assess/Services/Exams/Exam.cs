dded82110dc77f41c0286ab1589a603f807e36b8
Ada <kumrijasuada@gmail.com>
2024-04-14 16:27:46 +0200

    Exam finished.

--- Assess/Services/Exams/Exam.cs
+++ Assess/Services/Exams/Exam.cs
@@ -19,7 +19,6 @@
 
             return result;
         }
-
         // Write a program in C# that asks the user for 10 integers to store them in an array of integers
         // and show only even integers.
         public static void DisplayEvenNumbers(int[] numbers)
