91587a39efa59e49dd3419e5bb50caede9640f3a
Ada <kumrijasuada@gmail.com>
2024-04-17 12:04:01 +0200

    test

--- Assesment.Tests/UnitTests/Tests.cs
+++ Assesment.Tests/UnitTests/Tests.cs
@@ -48,6 +48,7 @@ namespace Assesment.Tests.UnitTests
                 Assert.That(Exam.IsPalindrome("level"), Is.True);
                 Assert.That(Exam.IsPalindrome("radar"), Is.True);
                 Assert.That(Exam.IsPalindrome("hello"), Is.False);
+                Assert.That(Exam.IsPalindrome("Hi"), Is.True);
                 Assert.That(Exam.IsPalindrome("Panama!"), Is.False);
                 Assert.That(Exam.IsPalindrome("!@#$%^&*()"), Is.True);
                 Assert.That(Exam.IsPalindrome("A!@#$%^&*()a"), Is.True);
