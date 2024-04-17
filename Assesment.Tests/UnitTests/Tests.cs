96d8b42d60747d751449cd6d83d51d8056ebeda6
Ada <kumrijasuada@gmail.com>
2024-04-17 11:48:55 +0200

    Exam

--- Assesment.Tests/UnitTests/Tests.cs
+++ Assesment.Tests/UnitTests/Tests.cs
@@ -31,7 +31,7 @@ namespace Assesment.Tests.UnitTests
             // Assert
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(120, result);
+                Assert.That(result, Is.EqualTo(120));
                 Assert.That(result, Is.EqualTo(120));
                 Assert.Throws<ArgumentException>(() => Exam.Factorial(-5), $"Factorial of {-5} should throw ArgumentException.");
             });
@@ -48,6 +48,7 @@ namespace Assesment.Tests.UnitTests
                 Assert.That(Exam.IsPalindrome("level"), Is.True);
                 Assert.That(Exam.IsPalindrome("radar"), Is.True);
                 Assert.That(Exam.IsPalindrome("hello"), Is.False);
+                Assert.That(Exam.IsPalindrome("test"), Is.True);
                 Assert.That(Exam.IsPalindrome("Panama!"), Is.False);
                 Assert.That(Exam.IsPalindrome("!@#$%^&*()"), Is.True);
                 Assert.That(Exam.IsPalindrome("A!@#$%^&*()a"), Is.True);
